7b14e6eb7a2ef944020f48c6de5f6b3f7d6b8a1a
Dhruv <dshivhare661@gmail.com>
2025-01-09 15:28:12 +0530

    navbar

--- project/app/views.py
+++ project/app/views.py
@@ -2,3 +2,18 @@ from django.shortcuts import render
 
 def landingpage(request):
     return render(request,'landingpage.html')
+
+def home(request):
+    return render(request,'home.html')
+
+def about(request):
+    return render(request,'about.html')
+
+def contact(request):
+    return render(request,'contact.html')
+
+def register(request):
+    return render(request,'register.html')
+
+def login(request):
+    return render(request,'login.html')
